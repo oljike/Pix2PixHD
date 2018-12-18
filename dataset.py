9652b183df38541f211f66bdc1ef6f299a75d250
olzhas.kabdolov <olzhas.kabdolov@btsdigital.kz>
2018-12-18 16:00:25 +0600

    Changed dataset

--- dataset.py
+++ dataset.py
@@ -6,12 +6,11 @@ import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 
 class DatasetFromFolder(Dataset):
-    def __init__(self, img_path, landmarks_path, transform=None):
+    def __init__(self, img_path, edges_path, transform=None):
         super(DatasetFromFolder, self).__init__()
 
-        #         self.img_path = glob.glob(os.path.join(img_path), '*.jpg')
-        self.landmarks_path = glob.glob(os.path.join(landmarks_path, '*.jpg'))
-        self.img_path = [os.path.join(img_path, os.path.basename(fname)) for fname in self.landmarks_path
+        self.edge_path = glob.glob(os.path.join(edges_path, '*.jpg'))
+        self.img_path = [os.path.join(img_path, os.path.basename(fname)) for fname in self.edge_path
                          if os.path.isfile(os.path.join(img_path, os.path.basename(fname)))]
 
         transform_list = [transforms.ToTensor()
@@ -20,17 +19,16 @@ class DatasetFromFolder(Dataset):
         self.transform = transforms.Compose(transform_list)
 
     def __getitem__(self, index):
-        # Load Image}
 
-        img = Image.open(self.img_path[index])  # .convert('RGB')
+        img = Image.open(self.img_path[index])
         img = img.resize((256, 128), Image.BICUBIC)
         img = self.transform(img)
 
-        landmark = Image.open(self.landmarks_path[index]).convert('L')
-        landmark = landmark.resize((256, 128), Image.BICUBIC)
-        landmark = self.transform(landmark)
+        edge = Image.open(self.edge_path[index]).convert('L')
+        edge = edge.resize((256, 128), Image.BICUBIC)
+        edge = self.transform(edge)
 
-        return img, landmark
+        return img, edge
 
     def __len__(self):
         return len(self.img_path)
